d3a63cea880eeeb670bacece12a0c5ddb224bb0c
James Xu <jamesxu_1996@hotmail.com>
2022-03-07 23:01:55 +1100

    set app to use postgresql

---
+++ app/controllers/collectibles_controller.rb
@@ -0,0 +1,23 @@
+class CollectiblesController < ApplicationController
+
+def index
+
+end
+
+def create
+
+end
+
+def show
+
+end
+
+def update
+
+end
+
+def destroy
+
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
 
 resources :collectibles
+root to: "collectibles#index"
+
+end
