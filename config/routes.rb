7a1af400b5a4e791a3897986db42711222dfdcfe
James Xu <jamesxu_1996@hotmail.com>
2022-03-08 08:40:31 +1100

    updated devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :collectibles
+  root to: "collectibles#index"
+
+
   devise_for :users
   get 'collectibles/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-resources :collectibles
-root to: "collectibles#index"
 
 end
