2e15a96329a47aa7dbacbc16d4ed15ccf7c2d04f
James Xu <jamesxu_1996@hotmail.com>
2022-03-13 01:51:44 +1100

    fixed routing, page restrictions if not signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
-  #before_action :authenticate_user!, only: [:restricted]
 
   def index
-  end
-
-  def restricted
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
-  get 'listings/index'
-  get 'listings/show'
-  get 'listings/new'
-  get 'listings/edit'
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/edit'
-  get 'profiles/index'
-  get 'profiles/show'
-  get 'profiles/new'
-  get 'profiles/edit'
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   resources :profiles, :reviews, :orders, :listings, :users
   root to: "home#index"
- 
-  get "/restricted", to: "collectibles#restricted"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
