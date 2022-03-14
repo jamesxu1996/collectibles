1765da186e3c7815e021c3c529599c5aae67f876
James Xu <jamesxu_1996@hotmail.com>
2022-03-14 23:06:40 +1100

    create CRUD actions for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,16 +1,45 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, 
 
   def index
     @listings = Listing.all
   end
 
   def show
+    @listing = Listing.find(params[:id])
   end
 
   def new
+    @listing = Listing.new
+  end
+
+  def create
+    listing = Listing.create(listing_params)
+
+    redirect_to listings_path(listing)
   end
 
   def edit
+    @listing = Listing.find(params[:id])
   end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update(listing_params)
+
+    redirect_to listing_path(@listing)
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+
+    redirect_to listings_path
+  end
+
+  private
+
+  #whitelisting parameters 
+  def listing_params
+    params.require(:listing).permit(:name, :price, :description, :quantity, :category)
 end
