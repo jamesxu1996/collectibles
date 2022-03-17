b431c69c53365a30b812d6e5986728c714613f27
James Xu <jamesxu_1996@hotmail.com>
2022-03-18 04:15:41 +1100

    added login/logout navbar, added stripe credential

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
-  resources :profiles, :reviews, :orders, :listings, :users
+  resources :profiles, :reviews, :orders, :listings
   root to: "home#index"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
