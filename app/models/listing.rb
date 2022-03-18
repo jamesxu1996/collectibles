cd0714d8dc2ef45fa4e8b53944eb8b12a4a0e180
James Xu <jamesxu_1996@hotmail.com>
2022-03-18 13:31:36 +1100

    added category model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,9 @@
 class Listing < ApplicationRecord
-    #paginates_per 5
+    belongs_to :user
+    belongs_to :category
+    has_one :order, dependent :destroy
+
+# sets conditions new, almost new, used, well loved
+    enum condition: {brand_new: 1, almost_new: 2, used: 3, well_loved: 4}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_12_121520) do
+ActiveRecord::Schema.define(version: 2022_03_18_022933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2022_03_12_121520) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["rateable_type", "rateable_id"], name: "index_average_caches_on_rateable"
     t.index ["rater_id"], name: "index_average_caches_on_rater_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "listings", force: :cascade do |t|
