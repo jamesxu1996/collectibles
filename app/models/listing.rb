29108d8b2c8f964e40ba1f35a963e21d6ac91c0f
James Xu <jamesxu_1996@hotmail.com>
2022-03-18 14:42:28 +1100

    created form for listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
     belongs_to :user
     belongs_to :category
-    has_one :order, dependent :destroy
+    has_one :order, dependent: :destroy
 
 # sets conditions new, almost new, used, well loved
     enum condition: {brand_new: 1, almost_new: 2, used: 3, well_loved: 4}
