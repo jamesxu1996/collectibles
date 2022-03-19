5083216f6fcbae8e6fdccc1f2a7ee4dce924db9e
James Xu <jamesxu_1996@hotmail.com>
2022-03-19 21:42:33 +1100

    fixed payments routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :profiles, :reviews, :orders, :listings
+  resources :profiles, :reviews, :orders, :listings, :payments
   root to: "home#index"
 
   post '/rate' => 'rater#create', :as => 'rate'
-  post 'payments/webhook', to: 'payments#webhook'
+
+  get "payments/success/:id", to: "payments#success", as: "payments_success"
+  post "payments/webhook", to: "payments#webhook"
+  post "payments", to: "payments#create_checkout_session", as: "create_checkout_session"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
