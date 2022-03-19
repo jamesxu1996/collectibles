9230af82b6046408fb787fa764cea234dc812d38
James Xu <jamesxu_1996@hotmail.com>
2022-03-19 19:55:19 +1100

    added secret signing key, added payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   resources :profiles, :reviews, :orders, :listings
   root to: "home#index"
-  
+
+  post '/rate' => 'rater#create', :as => 'rate'
+  post 'payments/webhook', to: 'payments#webhook'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
