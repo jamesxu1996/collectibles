a1a26c0445b98a54d0f533bbf399ff8f2204849a
James Xu <jamesxu_1996@hotmail.com>
2022-03-19 15:23:36 +1100

    fixed navbar redirections

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,7 +6,29 @@ class Listing < ApplicationRecord
     has_many :features, through: :listings_features
 
 
-# sets conditions new, almost new, used, well loved
+    # sets conditions new, almost new, used, well loved
     enum condition: {brand_new: 1, almost_new: 2, used: 3, well_loved: 4}
 
+    # data validations presence, name length, price numericality, description length
+    validates :name, presence: true, length: {minimum: 3}
+    validates :price, presence: true, numericality: {only_float: true}, length: {minimum: 1}
+    validates :description, presence: true, length: {maximum: 255}
+
+    # sanitise data
+    before_save :remove_whitespace
+    before_validation :convert_price_to_cents, if: :price_changed?
+
+
+    private
+
+    # removes whitespaces from user input data
+    def remove_whitespace
+        self.name = self.name.strip
+        self.description = self.description.strip
+    end
+
+    # conversion of set price to cents
+    def convert_price_to_cents
+        self.price = (self.attributes_before_type_cast["price"].to_f * 100)
+    end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
- belongs_to :listing
+ belongs_to :listing, foreign_key: "listing_id", dependent: :destroy
  belongs_to :buyer, foreign_key: "buyer_id", class_name: "User"
  belongs_to :seller, foreign_key: "seller_id", class_name: "User"
 end
