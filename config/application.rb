3c2e532ece7f2f3495d0155e1bfd6a00db21f999
James Xu <jamesxu_1996@hotmail.com>
2022-03-22 18:51:37 +1100

    added support for heroku environments

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Collectibles
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #allow support for different heroku environments
+    if ENV["PIPE_ENV"].present?
+      Rails.application.config.credentials.content_path = Rails.root.join("config/credentials/#{ENV["PIPE_ENV"]}.yml.enc")
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "payments/success/:id", to: "payments#success", as: "payments_success"
   post "payments/webhook", to: "payments#webhook"
   post "payments", to: "payments#create_checkout_session", as: "create_checkout_session"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
